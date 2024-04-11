265817c95893200ede445a5e0e827d295eec8885
mikeleo03 <leonmichael463@gmail.com>
2024-04-11 13:37:57 +0700

    refactor : add comments and log

--- Part A/RSA-dec.py
+++ Part A/RSA-dec.py
@@ -2,14 +2,15 @@ from sympy import factorint
 from Crypto.Util.number import *
 from mpmath import mp
 
-# Fungsi bantuan untuk menghitung pecahan lanjutan dari e/n
+# Helper function
+# Fungsi bantuan untuk menghitung bentuk pecahan dari e/n
 def continued_fraction(e, n):
     while n:
         a = e // n
         yield a
         e, n = n, e - a * n
 
-# Fungsi bantuan untuk mengubah pecahan lanjutan menjadi konvergen
+# Fungsi bantuan untuk mengubah pecahan menjadi konvergen
 def convergents_of_cont_frac(fraction):
     convergents = []
     for i in fraction:
@@ -22,22 +23,25 @@ def convergents_of_cont_frac(fraction):
     return convergents
 
 # RSA Decryption 
-# Pembangkitan kunci RSA tidak benar-benar acak, tetapi menggunakan sebuah bilangan prima yang diubah dengan nilai acak.
+# Pembangkitan kunci RSA tidak benar-benar acak, tetapi menggunakan sebuah bilangan prima yang diubah dengan nilai acak
 # Tidak aman dengan serangan faktorisasi
 def decrypt_variant_a(ciphertext, n, e):
     factors = factorint(n)
     p, q = factors.keys()
+    print("[*] Found p =", p)
+    print("[*] Found q =", q)
     d = inverse(e, (p - 1) * (q - 1))
     plaintext_int = pow(ciphertext, d, n)
     plaintext_bytes = long_to_bytes(plaintext_int)
     print("[*] Decryption successful with d =", d)
     return plaintext_bytes
 
-# Nilai N = p^k dengan k adalah bilangan asli
+# Nilai N = p^2, bentuk penggunaan modulus yang tidak aman
 # Jika N = p^k, maka Totient(N) = p^(k - 1) * (p - 1)
 def decrypt_variant_b(ciphertext, n, e):
     mp.dps = 1000
     p = int(mp.sqrt(n))
+    print("[*] Found p =", p)
     d = inverse(e, p * (p - 1))
     plaintext_int = pow(ciphertext, d, n)
     plaintext_bytes = long_to_bytes(plaintext_int)
@@ -45,18 +49,20 @@ def decrypt_variant_b(ciphertext, n, e):
     return plaintext_bytes
 
 # Nilai d yang sangat besar dan terbatas membuat konsekuensi nilai e kecil
-# Tidak aman dengan Wiener's attack
+# Tidak aman dengan serangan Wiener
 def decrypt_variant_c(ciphertext, n, e):
     # Hasilkan representasi pecahan lanjutan dari e/n
     frac = continued_fraction(e, n)
     
     # Ubah pecahan lanjutan menjadi konvergen
     convergents = convergents_of_cont_frac(frac)
+    print("[*] All convergent fraction =", convergents)
     
     # Mencoba untuk mendekripsi ciphertext menggunakan setiap konvergen sebagai eksponen privat
     for k, d in convergents:
         try:
             # Mencoba dekripsi
+            print("[*] Try to decrypt with d =", d)
             plaintext_int = pow(ciphertext, d, n)
             plaintext_bytes = long_to_bytes(plaintext_int)
             
@@ -73,11 +79,12 @@ def decrypt_variant_c(ciphertext, n, e):
     return None
 
 # Nilai e diketahui dan sangat kecil, e = 3
-# Tidak aman dengan Coppersmith's attack
+# Tidak aman dengan Cube Root attack
 def decrypt_variant_d(ciphertext, n, e):
     mp.dps = 1000
-    # Nilai e diketahui = 3, cukup hitung akar pangkat 3 nya
+    # Cukup hitung akar pangkat 3 nya
     plaintext_int = int(mp.cbrt(ciphertext))
+    print("[*] Integer plaintext =", plaintext_int)
     plaintext_bytes = long_to_bytes(plaintext_int)
     print("[*] Decryption successful without knowing d value")
     return plaintext_bytes
@@ -88,7 +95,7 @@ def decrypt_variant_e(ciphertext, n, e):
     d = inverse(e, (n - 1))
     plaintext_int = pow(ciphertext, d, n)
     plaintext_bytes = long_to_bytes(plaintext_int)
-    print("[*] Decryption successful without knowing d value")
+    print("[*] Decryption successful with d =", d)
     return plaintext_bytes
 
 # Main program
@@ -98,5 +105,5 @@ if __name__ == "__main__":
     e = int(input("Enter the public exponent (e): "))
     
     # Ganti varian nya
-    plaintext = decrypt_variant_c(c, n, e)
+    plaintext = decrypt_variant_e(c, n, e)
     print(plaintext)
